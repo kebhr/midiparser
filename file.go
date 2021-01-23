263547a225b7ddb6c76132bb490d431ea5148386
kebhr <42484226+kebhr@users.noreply.github.com>
2021-01-23 21:23:14 +0900

    feat: file.read

--- file.go
+++ file.go
@@ -6,8 +6,21 @@ import (
 
 type file struct {
 	file   *os.File
-	cursor uint
+	cursor int
 }
 
-func (*file) New(f *os.File) {
+func (f *file) Open(of *os.File) *file {
+	return &file{
+		file:   of,
+		cursor: 0,
+	}
+}
+
+func (f *file) read(n int) ([]byte, error) {
+	buf := make([]byte, n)
+	if _, err := f.file.Read(buf); err != nil {
+		return nil, err
+	}
+	f.cursor += n
+	return buf, nil
 }
